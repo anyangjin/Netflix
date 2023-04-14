ba55c16eac0881a78455cd36fdc6b55fcd584258
anyangjin <ayz0155@gmail.com>
2023-04-14 16:05:17 +0900

    requests.js 에서 필요한 요청들 가져오기

---
+++ src/api/requests.js
@@ -0,0 +1,13 @@
+const request = {
+  fecthNowPlaying: "movie/now_playing",
+  fecthNetflixOriginals: "discover/tv?with-networks=213",
+  fecthTrending: "trending/all/week",
+  fecthTopRated: "movie/top_rated",
+  fetchActionMovies: "discover/movie?with_genres=28",
+  fetchComedyMovies: "discover/movie?with_genres=35",
+  fetchHorrorMovies: "discover/movie?with_genres=27",
+  fetchRomanceMovies: "discover/movie?with_genres=10749",
+  fetchDocumentMovies: "discover/movie?with_genres=99",
+};
+
+export default request;
