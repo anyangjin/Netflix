9ce8bdc7f51d1853cdd632e97a59f63ab3432757
anyangjin <ayz0155@gmail.com>
2023-07-13 15:37:01 +0900

    nav상단 스크롤작동시 검은색으로 변경

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 export default function Nav() {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window.scrollY", window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
